79acc53c8109877124e090d9f4a9cdc4c7d238d5
Seb Bacon <seb.bacon@gmail.com>
2017-08-25 15:16:29 +0100

    Remove `_site` files that we don't need in VCS

--- _site/assets/js/company-tabs.js
+++
@@ -1,7 +0,0 @@
-(function($, AllTrials) {
-  $(function() {
-    AllTrials.companyTabs = $('.tabs').tabslet({
-      container: '#js-company-tabs'
-    });
-  });
-})(window.jQuery, window.AllTrials);
--- _site/test.py
+++
@@ -1,5 +0,0 @@
-from foo import Row
-
-asd = Row(['1','2','yes'])
-
-print [x.is_yes() for x in asd]
