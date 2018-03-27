96658a8a9d82c73ee5b18c941a89b673713d220d
Seb Bacon <seb.bacon@gmail.com>
2018-03-27 18:07:28 +0100

    Make trusts with apostrophies visited via search work
    Fixes #8

--- assets/js/search-autocomplete.js
+++ assets/js/search-autocomplete.js
@@ -3,6 +3,7 @@
   {
     return text.toString().toLowerCase()
       .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/'+/g, '-')           // Replace spaces with -
       .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
       .replace(/\-\-+/g, '-')         // Replace multiple - with single -
       .replace(/^-+/, '')             // Trim - from start of text
